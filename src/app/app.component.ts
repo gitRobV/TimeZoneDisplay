3831e0fa450a91c73f594d20501c960102f1614a
Robert Villarreal <robertv1979@gmail.com>
2017-08-09 15:47:01 -0700

    Initial and Final commit for TimeZone Display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,40 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'US Time Zone Display'
+  date_time = ''
+  active: String = ''
+
+    updateTimeZone(tz: String): void {
+        switch (tz) {
+            case 'pst':
+                this.date_time = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+                this.active = tz
+                break;
+            case 'mst':
+                this.date_time = new Date().toLocaleString("en-US", {timeZone: "America/Denver"});
+                this.active = tz
+                break;
+            case 'cst':
+                this.date_time = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"});
+                this.active = tz
+                break;
+            case 'est':
+                this.date_time = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+                this.active = tz
+                break;
+        }
+
+    }
+
+    clear(): void {
+        this.date_time = ''
+        this.active = ''
+    }
+
+
+
+
 }
